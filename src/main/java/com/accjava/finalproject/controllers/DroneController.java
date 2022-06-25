282cdef27539e7c6dd6275cbea9d6af67ea6b9c3
Leonardo Zaramello <leeurw@hotmail.com>
2022-06-25 05:21:09 -0300

    Add CRUD: drone

--- src/main/java/com/accjava/finalproject/controllers/DroneController.java
+++ src/main/java/com/accjava/finalproject/controllers/DroneController.java
@@ -1,15 +1,24 @@
 package com.accjava.finalproject.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accjava.finalproject.model.Drone;
 import com.accjava.finalproject.service.DroneService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/drones")
 public class DroneController {
@@ -20,5 +29,25 @@ public class DroneController {
   @GetMapping
   public List<Drone> getAllDrones() {
     return droneService.getAllDrones();
+  }
+
+  @PostMapping
+  public Drone createDrone(@RequestBody Drone drone) {
+    return droneService.createDrone(drone);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Drone> getAllDroneById(@PathVariable Long id) {
+    return droneService.getAllDroneById(id);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Drone> updateDrone(@PathVariable Long id, @RequestBody Drone drone) {
+    return droneService.updateDrone(id, drone);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Map<String, Boolean>> deleteDrone(@PathVariable Long id) {
+    return droneService.deleteDrone(id);
   }
 }
--- src/main/java/com/accjava/finalproject/service/DroneService.java
+++ src/main/java/com/accjava/finalproject/service/DroneService.java
@@ -1,10 +1,14 @@
 package com.accjava.finalproject.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.accjava.finalproject.exception.ResourceNotFoundException;
 import com.accjava.finalproject.model.Drone;
 import com.accjava.finalproject.repository.DroneRepository;
 
@@ -17,4 +21,39 @@ public class DroneService {
   public List<Drone> getAllDrones() {
     return droneRepository.findAll();
   }
+
+  public Drone createDrone(Drone drone) {
+    return droneRepository.save(drone);
+  }
+
+  public ResponseEntity<Drone> getAllDroneById(Long id) {
+    Drone droneFound = droneRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Drone doesn't exist with id: " + id));
+
+    return ResponseEntity.ok(droneFound);
+  }
+
+  public ResponseEntity<Drone> updateDrone(Long id, Drone drone) {
+    Drone droneFound = droneRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Drone doesn't exist with id: " + id));
+
+    droneFound.setMarca(drone.getMarca());
+    droneFound.setModelo(drone.getModelo());
+    
+    Drone updatedDrone = droneRepository.save(droneFound);
+
+    return ResponseEntity.ok(updatedDrone);
+  }
+
+  public ResponseEntity<Map<String, Boolean>> deleteDrone(Long id) {
+    Drone droneFound = droneRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Drone doesn't exist with id: " + id));
+
+    droneRepository.delete(droneFound);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("Deleted", Boolean.TRUE);
+
+    return ResponseEntity.ok(response);
+  }
+
+
+
+
 }
