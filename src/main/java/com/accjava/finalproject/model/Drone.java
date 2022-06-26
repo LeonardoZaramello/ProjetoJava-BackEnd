5907453c842a74aac61eab66d12512bbba140718
Leonardo Zaramello <leeurw@hotmail.com>
2022-06-26 00:03:44 -0300

    Fix: Entity drone

--- src/main/java/com/accjava/finalproject/model/Drone.java
+++ src/main/java/com/accjava/finalproject/model/Drone.java
@@ -10,11 +10,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
